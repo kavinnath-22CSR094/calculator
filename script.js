942d305d5dfe76e064661c2b75074de4c4dfd2e2
kavinnath-22CSR094 <arkavinnath@gmail.com>
2024-05-26 22:03:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let currentDisplay = document.getElementById('display').value;
+    document.getElementById('display').value = currentDisplay.slice(0, -1);
+}
+
+function calculate() {
+    let currentDisplay = document.getElementById('display').value;
+    try {
+        document.getElementById('display').value = eval(currentDisplay);
+    } catch (e) {
+        document.getElementById('display').value = 'Error';
+    }
+}
